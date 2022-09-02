e1d36aeaa3fa0c51968e427a52b837b884c2041c
cgrn24 <stlhnt42@gmail.com>
2022-09-02 23:10:19 +0700

    dop2

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Button name='GET DATA' callBack={GetMeData} />
+      <Button name='GET  DATA' callBack={GetMeData} />
       <ul>
         {data.map((el) => {
           return (
